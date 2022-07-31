dce3dea985c24f1f19a4b3b47f3d5744b02dd1fb
Perkles <edersombarros@hotmail.com>
2022-07-31 16:42:42 -0400

    classe de conexão com BD

---
+++ src/main/java/connection/SingleConnectionBanco.java
@@ -0,0 +1,47 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnectionBanco {
+
+	private static String banco = "jdbc:postgresql://localhost:5433/projeto.jsp?autoReconnect=true";
+	private static String user = "postgres";
+	private static String senha = "admin";
+	private static Connection connection = null;
+	
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+
+	static {
+		Conectar();
+	}
+
+	public SingleConnectionBanco() {// quando titer uma instancia vai conectar
+
+		Conectar();
+
+	}
+
+	private static void Conectar() {
+
+		try {
+
+			if (connection == null) {
+
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(senha, user, banco);
+				connection.setAutoCommit(false);
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();/* mostrar qualquer erro */
+
+		}
+
+	}
+
+}
--- src/main/java/filter/FilterAutenticacao.java
+++ src/main/java/filter/FilterAutenticacao.java
@@ -1,7 +1,10 @@
 package filter;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import connection.SingleConnectionBanco;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
@@ -14,14 +17,21 @@ import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
-@WebFilter(urlPatterns = { "/principal/*"}) /* intercepta todas as requisições que vierem do projeto ou mapeamento */
+@WebFilter(urlPatterns = { "/principal/*" }) /* intercepta todas as requisições que vierem do projeto ou mapeamento */
 public class FilterAutenticacao extends HttpFilter implements Filter {
+
+	private static Connection connection;
 
 	public FilterAutenticacao() {
 	}
 
 	/* encerra os processos quando o servidor é parado */
 	public void destroy() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/* Intercepta as requisições e as respostas no sistema */
@@ -29,32 +39,46 @@ public class FilterAutenticacao extends HttpFilter implements Filter {
 	/* Ex.: validações de autenticações */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+			try {
 
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpSession session = req.getSession();
+				HttpServletRequest req = (HttpServletRequest) request;
+				HttpSession session = req.getSession();
+	
+				String usuarioLogado = (String) session.getAttribute("usuario");
+	
+				String urlParaAutenticar = req.getServletPath();/* url está sendo acessada */
+	
+				/* validar se está logado senão redirediciona a tela de login */
+				if (usuarioLogado == null
+						&& !urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) {/* não está logado */
+	
+					RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutenticar);
+					request.setAttribute("msg", "Por favor realize o login!");
+					redireciona.forward(request, response);
+					return;/* Parar a execução a redireciona para o login */
+	
+				} else {
+	
+					chain.doFilter(request, response);
+				}
+	
+				connection.commit();/*deu tudo certo então commita as alterações no banco*/
 
-		String usuarioLogado = (String) session.getAttribute("usuario");
-
-		String urlParaAutenticar = req.getServletPath();/* url está sendo acessada */
-
-		/* validar se está logado senão redirediciona a tela de login */
-		if (usuarioLogado == null &&
-				!urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) {/* não está logado */
-
-			RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutenticar);
-			request.setAttribute("msg", "Por favor realize o login!");
-			redireciona.forward(request, response);
-			return;/* Parar a execução a redireciona para o login */
-
-		} else {
-
-			chain.doFilter(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 		}
 
 	}
 
 	/* Inicia os processos ou recursos qundo o servidor sobe o projeto */
 	public void init(FilterConfig fConfig) throws ServletException {
+
+		connection = SingleConnectionBanco.getConnection();
 	}
 
 }
